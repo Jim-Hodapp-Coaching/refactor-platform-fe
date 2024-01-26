563f9adef943c47ae3704edc5ec24f7f1507f7a9
Jim Hodapp <james.hodapp@gmail.com>
2024-01-25 19:05:25 -0600

    Doesn't work yet, but starting to add adding auth.js login functionality

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
-import { UserAuthForm } from "@/components/ui/user-auth-form"
+import { UserAuthForm } from "@/components/user-auth-form"
 
 export const metadata: Metadata = {
   title: "Authentication",
@@ -32,13 +32,13 @@ export default function AuthenticationPage() {
       </div>
       <div className="container relative hidden h-[800px] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <Link
-          href="/examples/authentication"
+          href="#"
           className={cn(
             buttonVariants({ variant: "ghost" }),
             "absolute right-4 top-4 md:right-8 md:top-8"
           )}
         >
-          Login
+          Sign Up
         </Link>
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
           <div className="absolute inset-0 bg-zinc-900" />
@@ -55,7 +55,7 @@ export default function AuthenticationPage() {
             >
               <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
             </svg>
-            Acme Inc
+            Refactor Coaching & Mentoring
           </div>
           <div className="relative z-20 mt-auto">
             <blockquote className="space-y-2">
@@ -72,10 +72,10 @@ export default function AuthenticationPage() {
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
+                Sign in to Refactor
               </h1>
               <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
+                Enter your email & password below to sign in
               </p>
             </div>
             <UserAuthForm />
--- src/components/user-auth-form.tsx
+++ src/components/user-auth-form.tsx
@@ -2,11 +2,43 @@
 
 import * as React from "react"
 
+import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+
 import { cn } from "@/lib/utils"
 import { Icons } from "@/components/ui/icons"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        const authResponse = await fetch("/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(credentials),
+        })
+
+        if (!authResponse.ok) {
+          return null
+        }
+
+        const user = await authResponse.json()
+
+        return user
+      },
+    }),
+  ],
+})
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
@@ -16,6 +48,8 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   async function onSubmit(event: React.SyntheticEvent) {
     event.preventDefault()
     setIsLoading(true)
+
+    await authorize()
 
     setTimeout(() => {
       setIsLoading(false)
@@ -37,6 +71,20 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
               autoCapitalize="none"
               autoComplete="email"
               autoCorrect="off"
+              required
+              disabled={isLoading}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="password">
+              Password
+            </Label>
+            <Input
+              id="password"
+              type="password"
+              autoCapitalize="none"
+              autoCorrect="off"
+              required
               disabled={isLoading}
             />
           </div>
