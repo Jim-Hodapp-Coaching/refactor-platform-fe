ceff85f4af1edbc8bb0f97aeb61e1458c2bf2221
David Agbenyega <david@herokode.com>
2024-06-27 14:06:38 -0400

    Reverse refactor

--- src/lib/api/coaching-relationships.ts
+++ src/lib/api/coaching-relationships.ts
@@ -1,40 +1,44 @@
 // Interacts with the coaching_relationship endpoints
 
-import { CoachingRelationshipWithUserNames,
-            coachingRelationshipsWithUserNamesToString,
-            defaultCoachingRelationshipsWithUserNames,
-            isCoachingRelationshipWithUserNamesArray
-        } from "@/types/coaching_relationship_with_user_names";
+import {
+  CoachingRelationshipWithUserNames,
+  coachingRelationshipsWithUserNamesToString,
+  defaultCoachingRelationshipsWithUserNames,
+  isCoachingRelationshipWithUserNamesArray
+} from "@/types/coaching_relationship_with_user_names";
 import { Id } from "@/types/general";
-import axios, { AxiosError, AxiosResponse, AxiosStatic } from "axios";
+import { AxiosError, AxiosResponse } from "axios";
 
 export const fetchCoachingRelationshipsWithUserNames = async (
-    organizationId: Id
-  ): Promise<[CoachingRelationshipWithUserNames[], string]> => {
-    const baseUrl = `http://localhost:4000/organizations/${organizationId}/coaching_relationships`;
-    const config = {
+  organizationId: Id
+): Promise<[CoachingRelationshipWithUserNames[], string]> => {
+  const axios = require("axios");
+
+  var relationships: CoachingRelationshipWithUserNames[] = defaultCoachingRelationshipsWithUserNames();
+  var err: string = "";
+
+  const data = await axios
+    .get(`http://localhost:4000/organizations/${organizationId}/coaching_relationships`, {
       withCredentials: true,
-      timeout: 5000,
+      setTimeout: 5000, // 5 seconds before timing out trying to log in with the backend
       headers: {
         "X-Version": "0.0.1",
       },
-    };
-    var relationships: CoachingRelationshipWithUserNames[] = defaultCoachingRelationshipsWithUserNames();
-    var err: string = "";
-
-    try {
-      const response: AxiosResponse = await axios.get(baseUrl, config);
-      console.debug(`AxiosResponse: ${response}`);
-
-      if(isCoachingRelationshipWithUserNamesArray(response.data.data)) {
+    })
+    .then(function (response: AxiosResponse) {
+      // handle success
+      console.debug(response);
+      if (isCoachingRelationshipWithUserNamesArray(response.data.data)) {
         relationships = response.data.data;
-        console.debug(`CoachingRelationshipsWithUserNames: ${coachingRelationshipsWithUserNamesToString(relationships)}.`);
+        console.debug(
+          `CoachingRelationshipsWithUserNames: ` + coachingRelationshipsWithUserNamesToString(relationships) + `.`
+        );
       }
-    } catch (error) {
-      const axiosError = error as AxiosError;
-      console.error(axiosError.response?.status);
-
-      if(axiosError.response?.status === 401) {
+    })
+    .catch(function (error: AxiosError) {
+      // handle error
+      console.error(error.response?.status);
+      if (error.response?.status == 401) {
         console.error("Retrieval of CoachingRelationshipsWithUserNames failed: unauthorized.");
         err = "Retrieval of CoachingRelationshipsWithUserNames failed: unauthorized.";
       } else {
@@ -43,8 +47,9 @@ export const fetchCoachingRelationshipsWithUserNames = async (
           `Retrieval of CoachingRelationshipsWithUserNames by organization Id (` + organizationId + `) failed.`
         );
         err =
-          `Retrieval of CoachingRelationshipsWithUserNames by organization Id (` + organizationId + `) failed.`;
+          `Retrieval of CoachingRealtionshipsWithUserNames by organization Id (` + organizationId + `) failed.`;
       }
-    } 
-    return [relationships, err];
-  };
+    });
+
+  return [relationships, err];
+};
