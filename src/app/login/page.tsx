003eec66d912eb096d40ad9af1bcc364533814cb
Jim Hodapp <james.hodapp@gmail.com>
2024-10-12 20:22:03 -0500

    Update the URL of the Refactor logo link

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -45,20 +45,8 @@ export default function AuthenticationPage() {
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
           <div className="absolute inset-0 bg-zinc-900" />
           <div className="relative z-20 flex items-center text-lg font-medium">
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-6 w-6"
-            >
-              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-            </svg> */}
             <Link
-              href="/dashboard"
+              href="https://www.refactorcoach.com"
               className="mr-2 flex items-center space-x-2"
             >
               <div
