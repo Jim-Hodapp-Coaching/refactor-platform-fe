1ad2df893a9755c5bb858185f66e34e751792cb2
David <david@herokode.com>
2024-10-30 22:37:59 -0400

    Slight wording change on session page

--- src/app/coaching-sessions/[id]/page.tsx
+++ src/app/coaching-sessions/[id]/page.tsx
@@ -193,7 +193,7 @@ export default function CoachingSessionsPage() {
                 to_Date: TO_DATE,
               }}
               onChange={handleSessionSelection}
-              placeholder="Select coaching session"
+              placeholder="Change coaching session"
               getOptionLabel={(session) => session.date.toString()}
               getOptionValue={(session) => session.id.toString()}
               elementId="session-selector"
