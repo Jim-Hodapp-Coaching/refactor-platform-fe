002ccbd778a3348dba583806401552770a552c18
Jim Hodapp <james.hodapp@gmail.com>
2024-12-16 10:03:58 -0600

    Dashboard page can't be server side rendered just yet

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Metadata } from "next";
 
 import * as React from "react";
@@ -7,10 +9,11 @@ import { cn } from "@/lib/utils";
 import { SelectCoachingSession } from "@/components/ui/dashboard/select-coaching-session";
 import { useAuthStore } from "@/lib/providers/auth-store-provider";
 
-export const metadata: Metadata = {
-  title: "Dashboard",
-  description: "Coaching dashboard",
-};
+// TODO: can't make this page be server side yet (not sure why on this page)
+// export const metadata: Metadata = {
+//   title: "Dashboard",
+//   description: "Coaching dashboard",
+// };
 
 function DashboardContainer({
   className,
