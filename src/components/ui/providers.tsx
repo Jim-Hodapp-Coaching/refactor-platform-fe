02ab644df1a99a745868cccfd272e6349d437f9b
Jim Hodapp <james.hodapp@gmail.com>
2024-12-23 11:30:32 -0600

    Fix production build error

--- src/components/ui/providers.tsx
+++ src/components/ui/providers.tsx
@@ -1,15 +1,21 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { ThemeProviderProps } from "next-themes/dist/types"
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-import { TooltipProvider } from "@/components/ui/tooltip"
+import { TooltipProvider } from "@/components/ui/tooltip";
+
+// Works around breaking change introduced in next-themes@0.4.1, see:
+// https://github.com/shadcn-ui/ui/issues/5706
+interface ThemeProviderProps {
+  children: React.ReactNode;
+  [key: string]: any;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider {...props}>
       <TooltipProvider>{children}</TooltipProvider>
     </NextThemesProvider>
-  )
+  );
 }
