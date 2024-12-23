8bb6d0705b294d17657a6279e23ca4ecf33eb737
Jim Hodapp <james.hodapp@gmail.com>
2024-12-22 20:24:11 -0600

    Upgrade to Next.js 15

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'standalone',
-    webpack: (config) => {
-		config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
-		return config;
-	}
+	experimental: {
+		turbo: {
+		  // ...
+		},
+	  },
+    // webpack: (config) => {
+	// 	config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
+	// 	return config;
+	// }
 };
 
 export default nextConfig;
